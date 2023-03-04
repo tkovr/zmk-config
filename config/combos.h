2fe4fd22cd64404f8961ef8324450fce07b296ff
takikoo <takiko@protonmail.com>
2023-03-04 19:29:18 +0100

    Adding combos, Key Å

---
+++ config/combos.h
@@ -0,0 +1,12 @@
+/ {
+    combos {
+        compatible = "zmk,combos";
+
+        combo_bspc {
+            timeout-ms = <50>;
+            key-positions = <9 10>;
+            bindings = <&kp BSPC>;
+        };
+
+    };
+};
