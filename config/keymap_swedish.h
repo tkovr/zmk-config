fb79e7a8893bf75ba3a26e1a6c57d7729980f32b
takikoo <takiko@protonmail.com>
2023-03-05 02:14:21 +0100

    Adding more layers

--- config/keymap_swedish.h
+++ config/keymap_swedish.h
@@ -16,7 +16,6 @@
 
 #pragma once
 
-//#include "keymap.h"
 #include <dt-bindings/zmk/keys.h>
 
 // clang-format off
@@ -35,57 +34,57 @@
  * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
  */
 // Row 1
-#define SE_SECT &kp GRAVE  // §
-#define SE_1    &kp N1    // 1
-#define SE_2    &kp N2    // 2
-#define SE_3    &kp N3    // 3
-#define SE_4    &kp N4    // 4
-#define SE_5    &kp N5    // 5
-#define SE_6    &kp N6    // 6
-#define SE_7    &kp N7    // 7
-#define SE_8    &kp N8    // 8
-#define SE_9    &kp N9    // 9
-#define SE_0    &kp N0    // 0
-#define SE_PLUS &kp MINUS // +
-#define SE_ACUT &kp EQUAL // ´ (dead)
+#define SE_SECT GRAVE  // §
+#define SE_1    N1    // 1
+#define SE_2    N2    // 2
+#define SE_3    N3    // 3
+#define SE_4    N4    // 4
+#define SE_5    N5    // 5
+#define SE_6    N6    // 6
+#define SE_7    N7    // 7
+#define SE_8    N8    // 8
+#define SE_9    N9    // 9
+#define SE_0    N0    // 0
+#define SE_PLUS MINUS // +
+#define SE_ACUT EQUAL // ´ (dead)
 // Row 2
-#define SE_Q    &kp Q    // Q
-#define SE_W    &kp W    // W
-#define SE_E    &kp E    // E
-#define SE_R    &kp R    // R
-#define SE_T    &kp T    // T
-#define SE_Y    &kp Y    // Y
-#define SE_U    &kp U    // U
-#define SE_I    &kp I    // I
-#define SE_O    &kp O    // O
-#define SE_P    &kp P    // P
-#define SE_ARNG &kp LBKT // Å
-#define SE_DIAE &kp RBKT // ¨ (dead)
+#define SE_Q    Q    // Q
+#define SE_W    W    // W
+#define SE_E    E    // E
+#define SE_R    R    // R
+#define SE_T    T    // T
+#define SE_Y    Y    // Y
+#define SE_U    U    // U
+#define SE_I    I    // I
+#define SE_O    O    // O
+#define SE_P    P    // P
+#define SE_ARNG LBKT // Å
+#define SE_DIAE RBKT // ¨ (dead)
 // Row 3
-#define SE_A    &kp A    // A
-#define SE_S    &kp S    // S
-#define SE_D    &kp D    // D
-#define SE_F    &kp F    // F
-#define SE_G    &kp G    // G
-#define SE_H    &kp H    // H
-#define SE_J    &kp J    // J
-#define SE_K    &kp K    // K
-#define SE_L    &kp L    // L
-#define SE_ODIA &kp SEMI // Ö
-#define SE_ADIA &kp SQT  // Ä
-#define SE_QUOT &kp NUHS // '
+#define SE_A    A    // A
+#define SE_S    S    // S
+#define SE_D    D    // D
+#define SE_F    F    // F
+#define SE_G    G    // G
+#define SE_H    H    // H
+#define SE_J    J    // J
+#define SE_K    K    // K
+#define SE_L    L    // L
+#define SE_ODIA SEMI // Ö
+#define SE_ADIA SQT  // Ä
+#define SE_QUOT NUHS // '
 // Row 4
-#define SE_LABK &kp NUBS // <
-#define SE_Z    &kp Z    // Z
-#define SE_X    &kp X    // X
-#define SE_C    &kp C    // C
-#define SE_V    &kp V    // V
-#define SE_B    &kp B    // B
-#define SE_N    &kp N    // N
-#define SE_M    &kp M    // M
-#define SE_COMM &kp COMMA// ,
-#define SE_DOT  &kp DOT  // .
-#define SE_MINS &kp SLASH// -
+#define SE_LABK NUBS // <
+#define SE_Z    Z    // Z
+#define SE_X    X    // X
+#define SE_C    C    // C
+#define SE_V    V    // V
+#define SE_B    B    // B
+#define SE_N    N    // N
+#define SE_M    M    // M
+#define SE_COMM COMMA// ,
+#define SE_DOT  DOT  // .
+#define SE_MINS SLASH// -
 
 /* Shifted symbols
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
