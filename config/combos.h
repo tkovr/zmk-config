c23a1125c2f30ff85a8dfec4cb3da8be3d950e38
takikoo <takiko@protonmail.com>
2023-03-09 00:42:58 +0100

    Combos updated

--- config/combos.h
+++ config/combos.h
@@ -4,32 +4,26 @@
 
         combo_bspc {
             timeout-ms = <50>;
-            key-positions = <16 17>;
+            key-positions = <31 32>;
             bindings = <&kp BSPC>;
         };
 
         combo_del {
             timeout-ms = <50>;
-            key-positions = <16 18>;
+            key-positions = <31 33>;
             bindings = <&kp DEL>;
         };
 
         combo_ret {
             timeout-ms = <50>;
-            key-positions = <27 28>;
+            key-positions = <25 26>;
             bindings = <&kp RET>;
-        };
-
-        combo_esc {
-            timeout-ms = <50>;
-            key-positions = <12 13>;
-            bindings = <&kp ESC>;
         };
 
         combo_col {
             timeout-ms = <50>;
             key-positions = <32 33>;
-            bindings = <&kp DOT>;
+            bindings = <&kp COLON>;
         };
 
     };
