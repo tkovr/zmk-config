9d95c5bc5d281e4a3f88964a493f8e414725233f
takikoo <takiko@protonmail.com>
2023-03-09 00:03:00 +0100

    Adding more combos

--- config/combos.h
+++ config/combos.h
@@ -4,8 +4,32 @@
 
         combo_bspc {
             timeout-ms = <50>;
-            key-positions = <9 10>;
+            key-positions = <16 17>;
             bindings = <&kp BSPC>;
+        };
+
+        combo_del {
+            timeout-ms = <50>;
+            key-positions = <16 18>;
+            bindings = <&kp DEL>;
+        };
+
+        combo_ret {
+            timeout-ms = <50>;
+            key-positions = <27 28>;
+            bindings = <&kp RET>;
+        };
+
+        combo_esc {
+            timeout-ms = <50>;
+            key-positions = <12 13>;
+            bindings = <&kp ESC>;
+        };
+
+        combo_col {
+            timeout-ms = <50>;
+            key-positions = <32 33>;
+            bindings = <&kp SE_DOT>;
         };
 
     };
