d6edbe8b8e71ba8b0b42b45eecf859f9edf032c1
takikoo <takiko@protonmail.com>
2023-03-28 00:27:54 +0200

    Adding gaming layer

--- config/combos.h
+++ config/combos.h
@@ -5,24 +5,28 @@
         combo_bspc {
             timeout-ms = <50>;
             key-positions = <31 32>;
+            layers = <0>
             bindings = <&kp BSPC>;
         };
 
         combo_del {
             timeout-ms = <50>;
             key-positions = <31 33>;
+            layers = <0>
             bindings = <&kp DEL>;
         };
 
         combo_ret {
             timeout-ms = <50>;
             key-positions = <25 26>;
+            layers = <0>
             bindings = <&kp RET>;
         };
 
         combo_col {
             timeout-ms = <50>;
             key-positions = <32 33>;
+            layers = <0>
             bindings = <&kp SE_COLN>;
         };
 
