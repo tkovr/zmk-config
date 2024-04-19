10ad508b4c1873adb15b37d585834468e6e819fc
takikoo <takiko@protonmail.com>
2024-04-19 03:15:07 +0200

    Moving definitions

---
+++ config/totem.h
@@ -0,0 +1,16 @@
+// LAYERS
+#define BASE 0
+#define NAV  1
+#define SYM  2
+#define ADJ  3
+#define TVP1 4
+#define TVP2 5
+
+// CUSTOM KEY CODES
+#define XXX &none
+#define ___ &trans
+
+#define MEH      LC(LS(LALT))
+#define TAB_PREV &kp LC(PG_UP)
+#define TAB_NEXT &kp LC(PG_DN)
+
