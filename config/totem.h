4477bdbe65d4dfa252a2694a12f09e11e9bc2455
takikoo <takiko@protonmail.com>
2024-04-22 00:51:00 +0200

    Adding some personal keymaps

--- config/totem.h
+++ config/totem.h
@@ -1,10 +1,15 @@
 // LAYERS
 #define BASE 0
-#define NAV  1
-#define SYM  2
-#define ADJ  3
-#define TVP1 4
-#define TVP2 5
+#define GAME 1
+#define NAV  2
+#define SYM  3
+#define ADJ  4
+
+#define BT_CH_0 = &bt BT_SEL 0
+#define BT_CH_1 = &bt BT_SEL 1
+#define BT_CH_2 = &bt BT_SEL 2
+#define BT_CH_3 = &bt BT_SEL 3
+#define BT_CH_4 = &bt BT_SEL 4
 
 // CUSTOM KEY CODES
 #define XXX &none
@@ -14,3 +19,4 @@
 #define TAB_PREV &kp LC(PG_UP)
 #define TAB_NEXT &kp LC(PG_DN)
 
+#define C_ESC  &mt LCTRL ESC
