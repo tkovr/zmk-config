0deff7fc5c44f21e99ec2d723132816871414231
takikoo <13720392+takikoo@users.noreply.github.com>
2024-05-23 00:13:53 +0200

    Removing more files related to corne-ish zen. Renaming some files

--- config/homerow_mods.h
+++ config/homerow_mods.h
@@ -1,7 +1,7 @@
 /* Homerow mods */
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5  // left-hand keys
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5  // right-hand keys
-#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // thumb keys
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 LB5  // left-hand keys
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 RB5  // right-hand keys
+#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                          // thumb keys
 
 /* left-hand HRMs */
 ZMK_BEHAVIOR(hml, hold_tap,
@@ -34,4 +34,3 @@ ZMK_BEHAVIOR(hmr, hold_tap,
 #define HRM_K  &hmr LCTRL K
 #define HRM_L  &hmr LALT L
 #define HRM_OE &hmr LGUI SE_ODIA
-
